644e294c3fa18bc8920f7179dc8e229e856e67c5
RMBarnard <rbarnard1107@gmail.com>
2025-07-16 18:40:26 -0600

    Bump version to 3.0.0

--- raresim/__init__.py
+++ raresim/__init__.py
@@ -4,5 +4,5 @@ This package provides tools for simulating and analyzing rare genetic variants
 in population genetics studies.
 """
 
-__version__ = "0.3.0"
+__version__ = "3.0.0"
 __all__ = ['__version__']
